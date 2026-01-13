5365fc12e4722ab543d6cfb88b4d31fdfe0693d4
Marccss123 <matiasurgiles28@gmail.com>
2026-01-12 19:25:30 -0500

    .

--- proyecto2/Producto.java
+++ proyecto2/Producto.java
@@ -33,7 +33,6 @@ public abstract class Producto {
     public String getSku() { return sku; }
     public void setSku(String sku) { this.sku = sku; }
 
-    public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
 
     public String getPlataforma() { return plataforma; }
